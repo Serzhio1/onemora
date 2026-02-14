a2e277a3f377ee84f42cd16fbb8e39f56009c4f8
serzhio <mahov.serezha2015@gmail.com>
2026-02-14 18:03:44 +0300

    fix: добавил два класса для event-driven подхода

---
+++ src/main/java/com/serzhio_pet_projects/onemora/events/EventsListener.java
@@ -0,0 +1,23 @@
+package com.serzhio_pet_projects.onemora.events;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.botapimethods.BotApiMethod;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+import java.io.Serializable;
+
+@Component
+@RequiredArgsConstructor
+public class EventsListener {
+
+    private final TelegramClient telegramClient;
+
+    @EventListener
+    public void on(MessageEvent event) throws TelegramApiException {
+        BotApiMethod<? extends Serializable> message = event.getMessage();
+        telegramClient.execute(message);
+    }
+}
